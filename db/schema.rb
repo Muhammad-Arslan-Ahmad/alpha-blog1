3d0b7ff1cb44a464831574cd79fc0782657a6a0c
Ammad Aslam <ammad.aslam@devbox.co>
2020-07-17 12:39:31 +0500

    secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_054406) do
+ActiveRecord::Schema.define(version: 2020_07_17_072006) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_054406) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
