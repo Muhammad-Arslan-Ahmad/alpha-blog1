909f27cba82b7136cf931767a5eb9e23586875f1
arslan <arslan.ahmad@devbox.co>
2020-07-20 15:50:09 +0500

    added many things from section 6 and moved helper methods to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+     before_action :set_user, only: [:show, :edit, :update]
 
     def new
       @user = User.new
@@ -8,7 +9,8 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            flash[:notice] = "You have successfully Signed up #{@user.username}"  
+             session[:user_id] = @user.id
+             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
             redirect_to articles_path
         else
             render 'new'
@@ -18,25 +20,37 @@ class UsersController < ApplicationController
    
 
     def edit
-       @user = User.find(params[:id])
-    
     end
 
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(user_params)
             flash[:notice] = "User Profile Updated"
-            redirect_to articles_path
+            redirect_to @user
 
        else 
             render 'edit'
        end  
     end
 
+    def show 
+      
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index 
+     
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
+
 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+        
+    def set_user
+        @user = User.find(params[:id])
     end
 
   end 
